48863c93eb14f44908410acab483804e153a8cf8
NicolasUnifio <272955@unifio.edu.br>
2026-09-15 19:03:21 +0000

    cheguei na metade

---
+++ src/app/modules/lista1/exercicio7/exercicio7.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-exercicio7',
+  styleUrl: './exercicio7.scss',
+  templateUrl: './exercicio7.html',
+})
+export class Exercicio7 {}
